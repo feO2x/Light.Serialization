fcaba3cacf5a8ccb832933ddae4462c5eee99c3c
Kenny Pflug <kenny.pflug@live.de>
2016-03-30 11:52:50 +0200

    added project Light.Serialization.Json, set version to 0.1.0

---
+++ Code/Light.Serialization.Json/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyTitle("Light.Serialization.Json")]
+[assembly: AssemblyDescription("A C# / .NET JSON serialization library where you don't need DTOs any longer.")]
+[assembly: AssemblyCompany("Kenny Pflug, Markus Webert")]
+[assembly: AssemblyProduct("Light.Serialization.Json")]
+[assembly: AssemblyCopyright("Copyright © 2016 Kenny Pflug, Markus Webert")]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: AssemblyVersion("0.1.0")]
