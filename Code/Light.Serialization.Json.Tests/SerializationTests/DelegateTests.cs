d37916768d5b4f3f1e6ec59a346bcbb80e1383b1
Kenny Pflug <kenny.pflug@live.de>
2016-04-01 14:51:25 +0200

    added DelegateTests

---
+++ Code/Light.Serialization.Json.Tests/SerializationTests/DelegateTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FluentAssertions;
+using Light.Serialization.Abstractions;
+using Xunit;
+
+namespace Light.Serialization.Json.Tests.SerializationTests
+{
+    public sealed class DelegateTests : BaseJsonSerializerTest
+    {
+        [Fact(DisplayName = "An exception is thrown when a delegate should be serialized as delegates are currently not supported.")]
+        public void ExceptionMustBeThrownWhenDelegateIsSerialized()
+        {
+            var numberOfCalls = 0;
+            var action = new Action(() => numberOfCalls++);
+
+            Action act = () => GetSerializedJson(action);
+
+            act.ShouldThrow<SerializationException>()
+               .And.Message.Should().Contain($"Type {action.GetType()} cannot be serialized because there is no IJsonWriterInstructor registered that can cover this type.");
+        }
+    }
+}
