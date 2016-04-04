c2bbb20b9cf0650b1699782be9a467743a8bb7c0
Kenny Pflug <kenny.pflug@live.de>
2016-04-04 14:20:09 +0200

    added RulesSupport for Unity Container, added test project for Light.Serialization.Json.Unity

---
+++ Code/Light.Serialization.Json.Unity.Tests/BaseJsonSerializerTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using FluentAssertions;
+using Light.Serialization.Abstractions;
+using Light.Serialization.Json.PrimitiveTypeFormatters;
+using Light.Serialization.Json.SerializationRules;
+using Light.Serialization.Json.Unity;
+using Microsoft.Practices.Unity;
+
+// ReSharper disable once CheckNamespace
+
+namespace Light.Serialization.Tests
+{
+    public abstract class BaseJsonSerializerTest
+    {
+        protected IUnityContainer Container;
+
+        protected BaseJsonSerializerTest()
+        {
+            Container = new UnityContainer().RegisterDefaultSerializationTypes();
+        }
+
+        protected void CompareJsonToExpected<T>(T value, string expected)
+        {
+            var json = GetSerializedJson(value);
+
+            json.Should().Be(expected);
+        }
+
+        protected void CompareHumanReadableJsonToExpected<T>(T value, string expected)
+        {
+            var json = GetSerializedHumanReadableJson(value);
+
+            json.Should().Be(expected);
+        }
+
+        protected string GetSerializedJson<T>(T value)
+        {
+            var jsonSerializer = Container.Resolve<ISerializer>();
+
+            return jsonSerializer.Serialize(value);
+        }
+
+        protected string GetSerializedHumanReadableJson<T>(T value)
+        {
+            Container.RegisterIndentingWhitespaceformatter();
+            var jsonSerializer = Container.Resolve<ISerializer>();
+
+            return jsonSerializer.Serialize(value);
+        }
+
+        protected void AddRule<T>(Action<Rule<T>> rule)
+        {
+            Container.WithSerializationRuleFor(rule);
+        }
+
+        protected void ReplaceTimeZoneInfoInDateTimeFormatter(TimeZoneInfo timeZoneInfo)
+        {
+            Container.RegisterType<IPrimitiveTypeFormatter, DateTimeFormatter>(typeof (DateTimeFormatter).Name,
+                                                                               new ContainerControlledLifetimeManager(), new InjectionFactory(c => new DateTimeFormatter
+                                                                                                                                                   {
+                                                                                                                                                       TimeZoneInfo = timeZoneInfo
+                                                                                                                                                   }));
+        }
+    }
+}
---
+++ Code/Light.Serialization.Json.Unity/RulesSupport.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Light.GuardClauses;
+using Light.Serialization.Json.SerializationRules;
+using Microsoft.Practices.Unity;
+
+namespace Light.Serialization.Json.Unity
+{
+    /// <summary>
+    /// Provides custom serialization rule support for the Unity container.
+    /// </summary>
+    public static class RulesSupport
+    {
+        /// <summary>
+        ///     Creates a serialization rule for the given type that is configured with the specified delegate.
+        /// </summary>
+        /// <typeparam name="T">The type that should be configured for serialization.</typeparam>
+        /// <param name="container">The Unity container.</param>
+        /// <param name="configureRule">The delegate that configures the actual formatter instance.</param>
+        /// <returns>The container for method chaining.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is null.</exception>
+        public static IUnityContainer WithSerializationRuleFor<T>(this IUnityContainer container, Action<Rule<T>> configureRule)
+        {
+            container.MustNotBeNull(nameof(container));
+            configureRule.MustNotBeNull(nameof(configureRule));
+
+            var newRule = container.Resolve<Rule<T>>();
+            configureRule(newRule);
+            var customInstructor = newRule.CreateInstructor();
+            container.RegisterInstance(typeof (T).FullName, customInstructor);
+            return container;
+        }
+
+        /// <summary>
+        ///     Configures the Unity container to use the specified rule for serialization.
+        /// </summary>
+        /// <typeparam name="T">The type that should be configured for serialization.</typeparam>
+        /// <param name="container">The Unity container.</param>
+        /// <param name="rule">The rule that describes how the type is serialized.</param>
+        /// <returns>The container for method chaining.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when any of the parameters is null.</exception>
+        public static IUnityContainer WithSerializationRuleFor<T>(this IUnityContainer container, Rule<T> rule)
+        {
+            container.MustNotBeNull(nameof(container));
+            rule.MustNotBeNull(nameof(rule));
+
+            var customInstructor = rule.CreateInstructor();
+            container.RegisterInstance(typeof (T).FullName, customInstructor);
+            return container;
+        }
+    }
+}
