e5ad5904d8d538ec93dd5cf844ebfaae6bd02358
Kenny Pflug <kenny.pflug@live.de>
2016-04-25 16:52:50 +0200

    added DateTimeParser

---
+++ Code/Light.Serialization.Json/TokenParsers/BaseIso8601DateTimeParser.cs
@@ -0,0 +1,75 @@
+using System;
+using Light.Serialization.Json.LowLevelReading;
+
+namespace Light.Serialization.Json.TokenParsers
+{
+    /// <summary>
+    ///     Represents the base class for JSON Token Parsers that deserialize ISO 8601 date time values (DateTimeParser and DateTimeOffsetParser).
+    /// </summary>
+    /// <typeparam name="T">The .NET type of the ISO 8601 duration.</typeparam>
+    public abstract class BaseIso8601DateTimeParser<T> : BaseJsonStringToPrimitiveParser<T>
+    {
+        /// <summary>
+        ///     Checks if the specified token is a JSON string and the requested type is <see cref="T" />.
+        /// </summary>
+        public bool IsSuitableFor(JsonToken token, Type requestedType)
+        {
+            return token.JsonType == JsonTokenType.String && requestedType == typeof(T);
+        }
+
+        protected static void ExpectCharacter(char character, JsonToken token, ref int currentIndex)
+        {
+            if (token[currentIndex++] != character)
+                throw CreateException(token);
+        }
+
+        protected static bool IsEndOfToken(int currentIndex, int tokenLength)
+        {
+            return currentIndex == tokenLength - 1;
+        }
+
+        protected static bool IsTimeZoneIndicator(JsonToken token, ref int currentIndex)
+        {
+            var character = token[currentIndex];
+            return character == 'Z' || character == '+' || character == '-';
+        }
+
+        protected static int ReadNumber(int expectedNumberOfDigits, JsonToken token, ref int currentIndex)
+        {
+            var result = 0;
+            for (var base10Position = expectedNumberOfDigits; base10Position > 0; base10Position--, currentIndex++)
+            {
+                var digit = GetDigit(token, currentIndex);
+                result += digit * CalculateBase(base10Position);
+            }
+            return result;
+        }
+
+        protected static int CalculateBase(int base10Position)
+        {
+            if (base10Position == 1)
+                return 1;
+
+            var result = 10;
+            for (var i = 2; i < base10Position; i++)
+            {
+                result *= 10;
+            }
+            return result;
+        }
+
+        protected static int GetDigit(JsonToken token, int currentIndex)
+        {
+            var character = token[currentIndex];
+            if (char.IsDigit(character) == false)
+                throw CreateException(token);
+
+            return character - '0';
+        }
+
+        protected static JsonDocumentException CreateException(JsonToken token, Exception innerException = null)
+        {
+            return new JsonDocumentException($"The specified token {token} does not represent a valid date time.", token, innerException);
+        }
+    }
+}
---
+++ Code/Light.Serialization.Json/TokenParsers/DateTimeParser.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Light.Serialization.Json.LowLevelReading;
+
+namespace Light.Serialization.Json.TokenParsers
+{
+    /// <summary>
+    ///     Represents a JSON token parser that can deserialize JSON strings in the ISO 8601 format to .NET date times.
+    /// </summary>
+    public sealed class DateTimeParser : BaseIso8601DateTimeParser<DateTime>, IJsonStringToPrimitiveParser
+    {
+        /// <summary>
+        ///     Gets or sets the date time kind that is used for short date values (without time component).
+        /// </summary>
+        public DateTimeKind DefaultDateTimeKind = DateTimeKind.Utc;
+
+        /// <summary>
+        ///     Gets the value indicating whether this parser can be cached.
+        /// </summary>
+        public bool CanBeCached => true;
+
+        /// <summary>
+        ///     Parses the given JSON string in ISO 8601 format to a .NET DateTime value.
+        /// </summary>
+        public object ParseValue(JsonDeserializationContext context)
+        {
+            return Parse(context.Token);
+        }
+
+        /// <summary>
+        ///     Tries to parse the given JSON string to a .NET DateTime.
+        /// </summary>
+        public PrimitiveParseResult TryParse(JsonToken token)
+        {
+            try
+            {
+                return new PrimitiveParseResult(true, Parse(token));
+            }
+            catch (JsonDocumentException)
+            {
+                return new PrimitiveParseResult(false);
+            }
+        }
+
+        private DateTime Parse(JsonToken token)
+        {
+            int year,
+                month,
+                day = 1,
+                hour = 0,
+                minute = 0,
+                second = 0,
+                millisecond = 0;
+
+            var currentIndex = 1;
+            var kind = DefaultDateTimeKind;
+
+            year = ReadNumber(4, token, ref currentIndex);
+            ExpectCharacter('-', token, ref currentIndex);
+            month = ReadNumber(2, token, ref currentIndex);
+            if (IsEndOfToken(currentIndex, token.Length))
+                goto CreateDateTime;
+
+            ExpectCharacter('-', token, ref currentIndex);
+            day = ReadNumber(2, token, ref currentIndex);
+            if (IsEndOfToken(currentIndex, token.Length))
+                goto CreateDateTime;
+
+            ExpectCharacter('T', token, ref currentIndex);
+            kind = DateTimeKind.Unspecified;
+            hour = ReadNumber(2, token, ref currentIndex);
+            if (IsEndOfToken(currentIndex, token.Length))
+                goto CreateDateTime;
+            if (IsTimeZoneIndicator(token, ref currentIndex))
+                goto CheckTimeZoneIndicator;
+
+            ExpectCharacter(':', token, ref currentIndex);
+            minute = ReadNumber(2, token, ref currentIndex);
+            if (IsEndOfToken(currentIndex, token.Length))
+                goto CreateDateTime;
+            if (IsTimeZoneIndicator(token, ref currentIndex))
+                goto CheckTimeZoneIndicator;
+
+            ExpectCharacter(':', token, ref currentIndex);
+            second = ReadNumber(2, token, ref currentIndex);
+            if (IsEndOfToken(currentIndex, token.Length))
+                goto CreateDateTime;
+            if (IsTimeZoneIndicator(token, ref currentIndex))
+                goto CheckTimeZoneIndicator;
+
+            ExpectCharacter('.', token, ref currentIndex);
+            millisecond = ReadNumber(3, token, ref currentIndex);
+            if (IsEndOfToken(currentIndex, token.Length))
+                goto CreateDateTime;
+
+            CheckTimeZoneIndicator:
+            var character = token[currentIndex++];
+            if (character == 'Z')
+                kind = DateTimeKind.Utc;
+            else if (character == '+' || character == '-')
+            {
+                kind = DateTimeKind.Local;
+                ReadNumber(2, token, ref currentIndex);
+                if (IsEndOfToken(currentIndex, token.Length))
+                    goto CreateDateTime;
+
+                ExpectCharacter(':', token, ref currentIndex);
+                ReadNumber(2, token, ref currentIndex);
+            }
+
+            if (IsEndOfToken(currentIndex, token.Length) == false)
+                throw CreateException(token);
+
+            CreateDateTime:
+            try
+            {
+                return new DateTime(year, month, day, hour, minute, second, millisecond, kind);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                throw CreateException(token, ex);
+            }
+        }
+    }
+}
