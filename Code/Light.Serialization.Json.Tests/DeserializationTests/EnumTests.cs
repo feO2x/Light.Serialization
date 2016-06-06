2c6e2d5c7d1db755f0fe7fc9a5e0beaede828ff3
Kenny Pflug <kenny.pflug@live.de>
2016-06-06 21:39:15 +0200

    added deserialization tests for enum values

---
+++ Code/Light.Serialization.Json.Tests/DeserializationTests/EnumTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO.Compression;
+using Xunit;
+
+namespace Light.Serialization.Json.Tests.DeserializationTests
+{
+    public sealed class EnumTests : BaseJsonDeserializerTest
+    {
+        [Theory(DisplayName = "The deserializer must be able to deserialize JSON strings to .NET enum values.")]
+        [InlineData("\"black\"", ConsoleColor.Black)]
+        [InlineData("\"insertLineBreaks\"", Base64FormattingOptions.InsertLineBreaks)]
+        [InlineData("\"noCompression\"", CompressionLevel.NoCompression)]
+        public void EnumValues<T>(string json, T expected)
+        {
+            CompareDeserializedJsonToExpected(json, expected);
+        }
+    }
+}
