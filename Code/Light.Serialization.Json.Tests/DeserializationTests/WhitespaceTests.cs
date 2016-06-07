c6f0b740693c70dd8e72657c6058e35a77f5d458
Kenny Pflug <kenny.pflug@live.de>
2016-06-07 09:22:30 +0200

    added deserialization tests for JSON documents containing whitespace

---
+++ Code/Light.Serialization.Json.Tests/DeserializationTests/WhitespaceTests.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+
+namespace Light.Serialization.Json.Tests.DeserializationTests
+{
+    public sealed class WhitespaceTests : BaseJsonDeserializerTest
+    {
+        [Theory(DisplayName = "Space before a single value must be ignored by the deserializer.")]
+        [InlineData(" 42", 42)]
+        [InlineData("  false", false)]
+        [InlineData("   true", true)]
+        [InlineData(" 0.8863", 0.8863)]
+        [InlineData("      \"Hello\"", "Hello")]
+        [InlineData("   null", null)]
+        public void SpacesBeforeSingleValueIsIgnoredCorrectly<T>(string json, T expected)
+        {
+            CompareDeserializedJsonToExpected(json, expected);
+        }
+
+        [Theory(DisplayName = "Space after a single value must be ignored by the deserializer.")]
+        [InlineData("-42 ", -42)]
+        [InlineData("true  ", true)]
+        [InlineData("false    ", false)]
+        [InlineData("3255.25 ", 3255.25)]
+        [InlineData("\"Hi there\"  ", "Hi there")]
+        [InlineData("null ", null)]
+        public void SpacesAfterSingleValueAreIgnoredCorrectly<T>(string json, T expected)
+        {
+            CompareDeserializedJsonToExpected(json, expected);
+        }
+    }
+}
