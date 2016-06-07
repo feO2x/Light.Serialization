6689dfbb19a8b05f20cc9d754a2de558cef5943d
Kenny Pflug <kenny.pflug@live.de>
2016-06-07 12:03:50 +0200

    added deserialization test for DTOs

---
+++ Code/Light.Serialization.Json.Tests/DeserializationTests/ComplexObjectTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Light.Serialization.Json.Tests.DeserializationTests
+{
+    public sealed class ComplexObjectTests : BaseJsonDeserializerTest
+    {
+        [Fact(DisplayName = "The deserializer must be able to deserialize a JSON document containing a complex object.")]
+        public void ComplexObject()
+        {
+            const string json = "{\"x\": 42, \"y\": \"Foo\"}";
+
+            var result = GetDeserializedJson<DummyClass>(json);
+
+            var expected = new DummyClass(42, "Foo");
+            result.ShouldBeEquivalentTo(expected);
+        }
+
+        public class DummyClass
+        {
+            public DummyClass(int x, string y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public int X { get; }
+
+            public string Y { get; }
+        }
+    }
+}
