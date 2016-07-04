9c709ec61df6152ec757d5e2614aa783debcc764
Kenny Pflug <kenny.pflug@live.de>
2016-07-04 21:04:53 +0200

    updated assemlby infos for v0.1

--- Code/Light.Serialization.Json/Properties/AssemblyInfo.cs
+++ Code/Light.Serialization.Json/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Resources;
 
 [assembly: AssemblyTitle("Light.Serialization.Json")]
-[assembly: AssemblyDescription("A C# / .NET JSON serialization library where you don't need DTOs any longer.")]
+[assembly: AssemblyDescription("A .NET JSON serialization library where you don't need DTOs any longer.")]
 [assembly: AssemblyCompany("Kenny Pflug, Markus Webert")]
 [assembly: AssemblyProduct("Light.Serialization.Json")]
 [assembly: AssemblyCopyright("Copyright © 2016 Kenny Pflug, Markus Webert")]
