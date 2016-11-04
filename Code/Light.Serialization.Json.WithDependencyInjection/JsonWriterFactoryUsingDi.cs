3da2cca4b23e182d0c81e62d499b6a7635ae6ebb
Kenny Pflug <kenny.pflug@live.de>
2016-11-04 15:01:26 +0100

    added XML comment for JsonWriterFactoryUsingDi

--- Code/Light.Serialization.Json.WithDependencyInjection/JsonWriterFactoryUsingDi.cs
+++ Code/Light.Serialization.Json.WithDependencyInjection/JsonWriterFactoryUsingDi.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Text;
 using Light.DependencyInjection;
 using Light.GuardClauses;
@@ -6,10 +7,18 @@ using Light.Serialization.Json.LowLevelWriting;
 
 namespace Light.Serialization.Json.WithDependencyInjection
 {
+    /// <summary>
+    ///     Represents an <see cref="IJsonWriterFactory" /> that utilizes a <see cref="DependencyInjectionContainer" /> to create <see cref="JsonWriter" /> instances.
+    /// </summary>
     public sealed class JsonWriterFactoryUsingDi : IJsonWriterFactory
     {
         private readonly DependencyInjectionContainer _container;
 
+        /// <summary>
+        ///     Initializes a new instance of <see cref="JsonWriterFactoryUsingDi" />.
+        /// </summary>
+        /// <param name="container">The container used to resolve dependencies for <see cref="JsonWriter" /> instances.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="container" /> is null.</exception>
         public JsonWriterFactoryUsingDi(DependencyInjectionContainer container)
         {
             container.MustNotBeNull(nameof(container));
@@ -17,6 +26,7 @@ namespace Light.Serialization.Json.WithDependencyInjection
             _container = container;
         }
 
+        /// <inheritdoc />
         public IJsonWriter CreateFromStringBuilder(StringBuilder stringBuilder)
         {
             return new JsonWriter(new StringBuilderAdapter(stringBuilder),
@@ -24,6 +34,7 @@ namespace Light.Serialization.Json.WithDependencyInjection
                                   _container.Resolve<IJsonKeyNormalizer>());
         }
 
+        /// <inheritdoc />
         public IJsonWriter CreateFromTextWriter(TextWriter writer)
         {
             return new JsonWriter(new TextWriterAdapter(writer),
@@ -31,6 +42,7 @@ namespace Light.Serialization.Json.WithDependencyInjection
                                   _container.Resolve<IJsonKeyNormalizer>());
         }
 
+        /// <inheritdoc />
         public IJsonWriter CreateFromBinaryWriter(BinaryWriter writer)
         {
             return new JsonWriter(new BinaryWriterAdapter(writer),
