141f3ed949964c74815027e4671498a064b08ca1
Kenny Pflug <kenny.pflug@live.de>
2016-11-04 14:50:37 +0100

    added XML comments for DependencyInjectionMetaFactory

--- Code/Light.Serialization.Json.WithDependencyInjection/DependencyInjectionMetaFactory.cs
+++ Code/Light.Serialization.Json.WithDependencyInjection/DependencyInjectionMetaFactory.cs
@@ -7,10 +7,17 @@ using Light.Serialization.Json.ComplexTypeConstruction;
 
 namespace Light.Serialization.Json.WithDependencyInjection
 {
+    /// <summary>
+    ///     Represents an <see cref="IMetaFactory" /> that utilizes a <see cref="DependencyInjectionContainer" /> to instantiate dictionaries and collections.
+    /// </summary>
     public sealed class DependencyInjectionMetaFactory : IMetaFactory
     {
         private readonly DependencyInjectionContainer _container;
 
+        /// <summary>
+        ///     Initializes a new instance of <see cref="DependencyInjectionMetaFactory" />.
+        /// </summary>
+        /// <param name="container">The DI container that is used to instantiate dictionaries and collections.</param>
         public DependencyInjectionMetaFactory(DependencyInjectionContainer container)
         {
             container.MustNotBeNull(nameof(container));
@@ -18,16 +25,22 @@ namespace Light.Serialization.Json.WithDependencyInjection
             _container = container;
         }
 
+        /// <inheritdoc />
         public IDictionary CreateDictionary(Type dictionaryTypeToConstruct)
         {
             return (IDictionary) _container.Resolve(dictionaryTypeToConstruct);
         }
 
+        /// <inheritdoc />
         public IList CreateCollection(Type collectionTypeToConstruct)
         {
             return (IList) _container.Resolve(collectionTypeToConstruct);
         }
 
+        /// <summary>
+        ///     Throws a <see cref="NotSupportedException" /> because the <see cref="ComplexObjectParserUsingDi" /> does not call this method.
+        /// </summary>
+        /// <exception cref="NotSupportedException">Thrown when this method is called.</exception>
         public object CreateObject(TypeCreationDescription typeCreationDescription, Dictionary<InjectableValueDescription, InjectableValue> deserializedChildValues)
         {
             throw new NotSupportedException("Objects should not be created using the DependencyInjectionMetaFactory.");
